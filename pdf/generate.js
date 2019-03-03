4b0e4c7f4aeffbe9aca9e3a8af32c1af900cfabf
Damian Frizzi <damian.f@v-designs.ch>
2019-03-03 16:07:22 +0100

    Added logging

--- pdf/generate.js
+++ pdf/generate.js
@@ -14,38 +14,65 @@ const pathOnFileSystem = '/tmp/pdf/dev.pdf'
  */
 const genreatePdf = async (url, path = pathOnFileSystem, launchOptions) => {
   try {
-    const stats = fs.statSync(path)
-    const lastModified = (new Date().getTime() - new Date(stats.mtime).getTime()) / 1000
-
-    if (stats.isFile && lastModified <= fileSystemCacheTTL) {
-      const file = fs.readFileSync(path)
-      return file
-    } else {
-      throw 'Cache miss'
-    }
-  } catch (_err) {
-    const browser = await puppeteer.launch(
-      launchOptions || {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless
-      }
-    )
-    const page = await browser.newPage()
-
-    /** @type {import('puppeteer-core').PDFOptions} */
-    const config = {
-      format: 'A4',
-      path,
-      printBackground: true
-    }
-
-    await page.goto(url, { waitUntil: 'networkidle2' })
-    const file = await page.pdf(config)
-
-    await browser.close()
-    return file
+    return await getFromFileSystem(path)
+  } catch (err) {
+    console.log(err)
+    return await getFromBrowser(url, path, launchOptions)
   }
+}
+
+/** @param {string} path */
+const getFromFileSystem = path => {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err, stats) => {
+      if (err || !stats.isFile) {
+        reject(err.message)
+      } else {
+        const lastModified = (new Date().getTime() - new Date(stats.mtime).getTime()) / 1000
+
+        if (stats.isFile && lastModified <= fileSystemCacheTTL) {
+          fs.readFile(path, (err, data) => {
+            if (err) {
+              reject(err.message)
+            }
+
+            resolve(data)
+          })
+        } else {
+          reject('cache miss')
+        }
+      }
+    })
+  })
+}
+
+/**
+ * @param {string} url
+ * @param {string} [path]
+ * @param {ILaunchOptions} [launchOptions]
+ */
+const getFromBrowser = async (url, path, launchOptions) => {
+  const browser = await puppeteer.launch(
+    launchOptions || {
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless
+    }
+  )
+  const page = await browser.newPage()
+
+  /** @type {import('puppeteer-core').PDFOptions} */
+  const config = {
+    format: 'A4',
+    path,
+    printBackground: true
+  }
+
+  await page.goto(url, { waitUntil: 'networkidle2' })
+  const file = await page.pdf(config)
+
+  await browser.close()
+  return file
 }
 
 module.exports = { genreatePdf }

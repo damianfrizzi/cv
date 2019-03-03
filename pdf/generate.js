07395c1d0470ea9130111caf0f6259cb77f72fec
Damian Frizzi <damian.f@v-designs.ch>
2019-03-03 16:21:25 +0100

    Don't write to disc

--- pdf/generate.js
+++ pdf/generate.js
@@ -1,9 +1,9 @@
 // @ts-check
-const fs = require('fs')
+// const fs = require('fs')
 const chrome = require('chrome-aws-lambda')
 const puppeteer = require('puppeteer-core')
 
-const fileSystemCacheTTL = 3600 // seconds
+// const fileSystemCacheTTL = 3600 // seconds
 const pathOnFileSystem = '/tmp/pdf/dev.pdf'
 
 /**
@@ -12,41 +12,39 @@ const pathOnFileSystem = '/tmp/pdf/dev.pdf'
  * @param {string} [path]
  * @param {ILaunchOptions} [launchOptions]
  */
-const genreatePdf = (url, path = pathOnFileSystem, launchOptions) => {
-  return getFromFileSystem(path).catch(_err => getFromBrowser(url, path, launchOptions))
-}
+const genreatePdf = (url, path = pathOnFileSystem, launchOptions) => getFromBrowser(url, path, launchOptions)
 
 /** @param {string} path */
-const getFromFileSystem = path => {
-  return new Promise((resolve, reject) => {
-    fs.stat(path, (err, stats) => {
-      if (err || !stats.isFile) {
-        reject(err.message)
-      } else {
-        const lastModified = (new Date().getTime() - new Date(stats.mtime).getTime()) / 1000
+// const getFromFileSystem = path => {
+//   return new Promise((resolve, reject) => {
+//     fs.stat(path, (err, stats) => {
+//       if (err || !stats.isFile) {
+//         reject(err.message)
+//       } else {
+//         const lastModified = (new Date().getTime() - new Date(stats.mtime).getTime()) / 1000
 
-        if (stats.isFile && lastModified <= fileSystemCacheTTL) {
-          fs.readFile(path, (err, data) => {
-            if (err) {
-              reject(err.message)
-            }
+//         if (stats.isFile && lastModified <= fileSystemCacheTTL) {
+//           fs.readFile(path, (err, data) => {
+//             if (err) {
+//               reject(err.message)
+//             }
 
-            resolve(data)
-          })
-        } else {
-          reject('cache miss')
-        }
-      }
-    })
-  })
-}
+//             resolve(data)
+//           })
+//         } else {
+//           reject('cache miss')
+//         }
+//       }
+//     })
+//   })
+// }
 
 /**
  * @param {string} url
- * @param {string} [path]
+ * @param {string} [_path]
  * @param {ILaunchOptions} [launchOptions]
  */
-const getFromBrowser = async (url, path, launchOptions) => {
+const getFromBrowser = async (url, _path, launchOptions) => {
   const browser = await puppeteer.launch(
     launchOptions || {
       args: chrome.args,
@@ -59,7 +57,6 @@ const getFromBrowser = async (url, path, launchOptions) => {
   /** @type {import('puppeteer-core').PDFOptions} */
   const config = {
     format: 'A4',
-    path,
     printBackground: true
   }
 

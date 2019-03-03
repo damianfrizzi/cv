137e439d4a8f978740f4066541546d191a4aed8b
Damian Frizzi <damian.f@v-designs.ch>
2019-03-03 13:50:33 +0100

    Hardcode screenshot url

--- screenshot.js
+++ screenshot.js
@@ -1,23 +1,16 @@
-const { parse } = require('url')
 const { getScreenshot } = require('./chromium')
-const { getInt, getUrlFromPath, isValidUrl } = require('./validator')
 
-module.exports = async function(req, res) {
+module.exports = /**
+ * @param {import('http').IncomingMessage} req
+ * @param {import('http').ServerResponse} res
+ */ async function(req, res) {
   try {
-    const { pathname = '/', query = {} } = parse(req.url, true)
-    const { type = 'png', quality, fullPage } = query
-    const url = getUrlFromPath(pathname)
-    const qual = getInt(quality)
-    if (!isValidUrl(url)) {
-      res.statusCode = 400
-      res.setHeader('Content-Type', 'text/html')
-      res.end(`<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`)
-    } else {
-      const file = await getScreenshot(url, type, qual, fullPage)
-      res.statusCode = 200
-      res.setHeader('Content-Type', `image/${type}`)
-      res.end(file)
-    }
+    const type = 'png'
+    const url = 'https://dafri.io'
+    const file = await getScreenshot(url, type, undefined, undefined)
+    res.statusCode = 200
+    res.setHeader('Content-Type', `image/${type}`)
+    res.end(file)
   } catch (e) {
     res.statusCode = 500
     res.setHeader('Content-Type', 'text/html')

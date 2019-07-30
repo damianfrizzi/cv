37e63a00ccf212ff55c870559e68e137d6a6268f
damianfrizzi <damian.f@v-designs.ch>
2019-07-30 22:13:59 +0200

    Added manifest and route to now.json

--- next.config.js
+++ next.config.js
@@ -1,1 +1,22 @@
-module.exports = require('next-offline')()
+module.exports = require('next-offline')({
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
+})

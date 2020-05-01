3d4e5e43fb88cf52b35afcf41cae04f326641e41
Damian Frizzi <damian.frizzi@truewealth.ch>
2020-05-01 22:48:43 +0200

    Improve PWA

--- next.config.js
+++ next.config.js
@@ -1,39 +1,19 @@
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 const withOffline = require('next-offline')
 
-module.exports = withBundleAnalyzer({
-  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/server.html'
+module.exports = withOffline(
+  withBundleAnalyzer({
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/server.html',
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html',
+      },
     },
-    browser: {
-      analyzerMode: 'static',
-      reportFilename: '../bundles/client.html'
-    }
-  },
-  ...withOffline({
-    workboxOpts: {
-      swDest: 'static/service-worker.js',
-      runtimeCaching: [
-        {
-          urlPattern: /^https?.*/,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'https-calls',
-            networkTimeoutSeconds: 15,
-            expiration: {
-              maxEntries: 150,
-              maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-            },
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }
-      ]
-    }
   })
-})
+)

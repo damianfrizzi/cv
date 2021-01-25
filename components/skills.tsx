b5454aefc511207e361c5bb6e7814a5ddd0a8af4
Damian Frizzi <damian.frizzi@gmail.com>
2021-01-25 22:24:57 +0100

    Improve readability

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,9 +1,9 @@
-import { PrismicDocument, PrismicSkill } from 'lib/prismic/types'
-import { FC } from 'react'
-import { CardGrid } from './cardGrid'
-import { Anchors, Section } from './section'
-import { Card } from './card'
-import styles from './skills.module.scss'
+import { PrismicDocument, PrismicSkill } from "lib/prismic/types"
+import { FC } from "react"
+import { CardGrid } from "./cardGrid"
+import { Anchors, Section } from "./section"
+import { Card } from "./card"
+import styles from "./skills.module.scss"
 
 interface SkillsProps {
   skills: PrismicDocument<PrismicSkill>[]
@@ -12,25 +12,19 @@ interface SkillsProps {
 export const Skills: FC<SkillsProps> = ({ skills }) => (
   <Section title={Anchors.Skills}>
     <CardGrid numOfCells={2}>
-      {skills.map(({ data }, _i) => (
-        <Card key={data.title}>
-          <h3>{data.title}</h3>
+      {skills.map(({ data }, _i) => {
+        const items = [...(data?.proficiency ?? []), ...(data?.basics ?? [])]
 
-          {data.proficiency && data.proficiency.length > 0 && (
-            <div className={styles.skillBlock}>
-              <div className={styles.title}>Proficiency:</div>
-              <div className={styles.list}>{data.proficiency.map((p) => p.skill).join(', ')}</div>
-            </div>
-          )}
+        return (
+          <Card key={data.title}>
+            <h3>{data.title}</h3>
 
-          {data.basics && data.basics?.length > 0 && (
             <div className={styles.skillBlock}>
-              <div className={styles.title}>Basics:</div>
-              <div className={styles.list}>{data.basics.map((b) => b.skill).join(', ')}</div>
+              <div className={styles.list}>{items.map((p) => p.skill).join(", ")}</div>
             </div>
-          )}
-        </Card>
-      ))}
+          </Card>
+        )
+      })}
     </CardGrid>
   </Section>
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
-import { Education } from 'components/education'
-import { Experience } from 'components/experience'
-import { Header } from 'components/header'
-import { Languages } from 'components/languages'
-import { Main } from 'components/main'
-import { Skills } from 'components/skills'
-import { Stripe, Stripes } from 'components/stripe'
-import { PrismicClient } from 'lib/prismic/config'
-import { PrismicDocument, PrismicEducation, PrismicLanguage, PrismicPosition, PrismicSkill } from 'lib/prismic/types'
-import { GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
-import Prismic from 'prismic-javascript'
+import { Education } from "components/education"
+import { Experience } from "components/experience"
+import { Header } from "components/header"
+import { Languages } from "components/languages"
+import { Main } from "components/main"
+import { Skills } from "components/skills"
+import { PrismicClient } from "lib/prismic/config"
+import { PrismicDocument, PrismicEducation, PrismicLanguage, PrismicPosition, PrismicSkill } from "lib/prismic/types"
+import { GetStaticProps, NextPage } from "next"
+import Head from "next/head"
+import Prismic from "prismic-javascript"
 
 interface HomePageProps {
   positions: PrismicDocument<PrismicPosition>[]
@@ -23,13 +22,11 @@ const HomePage: NextPage<HomePageProps> = ({ positions, educations, languages, s
     <Head>
       <title>Damian Frizzi - Frontend Engineer</title>
     </Head>
-    <Stripe position={Stripes.Top} />
     <Header />
     <Experience positions={positions} />
     <Education educations={educations} />
     <Skills skills={skills} />
     <Languages languages={languages} />
-    <Stripe position={Stripes.Bottom} />
   </Main>
 )
 
@@ -37,23 +34,23 @@ export const getStaticProps: GetStaticProps = async ({ preview = false, previewD
   const prismicAPI = await PrismicClient.getApi()
   const ref = previewData?.ref || prismicAPI.masterRef.ref
 
-  const resPositions = await PrismicClient.query(Prismic.Predicates.at('document.type', 'position'), {
+  const resPositions = await PrismicClient.query(Prismic.Predicates.at("document.type", "position"), {
     ref,
-    orderings: '[my.position.start_date desc]',
+    orderings: "[my.position.start_date desc]",
   })
 
-  const resEducations = await PrismicClient.query(Prismic.Predicates.at('document.type', 'education'), {
+  const resEducations = await PrismicClient.query(Prismic.Predicates.at("document.type", "education"), {
     ref,
-    orderings: '[my.education.start_date desc]',
+    orderings: "[my.education.start_date desc]",
   })
 
-  const resLanguages = await PrismicClient.query(Prismic.Predicates.at('document.type', 'language'), {
+  const resLanguages = await PrismicClient.query(Prismic.Predicates.at("document.type", "language"), {
     ref,
   })
 
-  const resSkills = await PrismicClient.query(Prismic.Predicates.at('document.type', 'skill'), {
+  const resSkills = await PrismicClient.query(Prismic.Predicates.at("document.type", "skill"), {
     ref,
-    orderings: '[my.skill.progress desc]',
+    orderings: "[my.skill.progress desc]",
   })
 
   return {
